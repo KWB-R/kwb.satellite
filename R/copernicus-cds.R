ce8e664b1a93561ad2df6d7dfffc5aca25ae9b5c
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-03-12 15:39:44 +0100

    Reflow comments, reindent code (nothing else!)

--- R/copernicus-cds.R
+++ R/copernicus-cds.R
@@ -1,15 +1,17 @@
 
 #' Copernicus Climate Data Store: Single Query
 #'
-#' @description Run request (may take a while as nothing happens because many requests R
-#' are queued (in addition: maximum request data amount limited to 140000
-#' data points).
-#' To check queue status: "https://cds.climate.copernicus.eu/cdsapp#!/yourrequests?tab=form"
+#' @description Run request (may take a while as nothing happens because many
+#'   requests R are queued (in addition: maximum request data amount limited to
+#'   140000 data points). To check queue status:
+#'   "https://cds.climate.copernicus.eu/cdsapp#!/yourrequests?tab=form"
 #' @param variable variable to query "2m_temperature"
 #' @param dataset_short_name "reanalysis-era5-single-levels"
 #' @param product_type  default: "reanalysis"
-#' @param years character vector of years (default:  as.character(seq(2010, 2020)))
-#' @param area area coordinates in latitude/longitude (default: c(40, 116, 39, 117))
+#' @param years character vector of years (default:  as.character(seq(2010,
+#'   2020)))
+#' @param area area coordinates in latitude/longitude (default: c(40, 116, 39,
+#'   117))
 #' @param file_format "grib" or "netcdf"
 #' @param export_dir default: "."
 #'
@@ -17,16 +19,16 @@
 #' @export
 #' @importFrom ecmwfr wf_request
 #'
-copernicus_cds <- function(variable = "2m_temperature",
-                           dataset_short_name = "reanalysis-era5-single-levels",
-                           product_type = "reanalysis",
-                           years = as.character(seq(2010, 2020)),
-                           area = c(40, 116, 39, 117),
-                           file_format = "grib",
-                           export_dir = ".") {
-
-
-
+copernicus_cds <- function(
+  variable = "2m_temperature",
+  dataset_short_name = "reanalysis-era5-single-levels",
+  product_type = "reanalysis",
+  years = as.character(seq(2010, 2020)),
+  area = c(40, 116, 39, 117),
+  file_format = "grib",
+  export_dir = "."
+)
+{
   request <- list(
     product_type = product_type,
     format = file_format,
@@ -42,32 +44,35 @@ copernicus_cds <- function(variable = "2m_temperature",
              "21:00", "22:00", "23:00"),
     area = area,
     dataset_short_name = dataset_short_name,
-    target =  sprintf("%s_%s_%s-%s.%s",
-                      dataset_short_name,
-                      variable,
-                      min(as.numeric(years)),
-                      max(as.numeric(years)),
-                      file_format))
+    target =  sprintf(
+      "%s_%s_%s-%s.%s",
+      dataset_short_name,
+      variable,
+      min(as.numeric(years)),
+      max(as.numeric(years)),
+      file_format)
+  )
 
-
-  ecmwfr::wf_request(request = request,
-                     transfer = TRUE,
-                     path = export_dir,
-                     verbose = FALSE)
-
+  ecmwfr::wf_request(
+    request = request,
+    transfer = TRUE,
+    path = export_dir,
+    verbose = FALSE
+  )
 }
 
-
 #' Copernicus Climate Data Store: Multi Query
-#' @description Runs \code{copernicus_cds} in parallel for all variables
-#' defined in parameter "variables" on all machine cores minus one
-#' @param variables default: c("2m_temperature", "evaporation", "potential_evaporation",
-#' "precipitation_type", "runoff", "sub_surface_runoff", "surface_runoff",
-#' "total_precipitation")
+#' @description Runs \code{copernicus_cds} in parallel for all variables defined
+#'   in parameter "variables" on all machine cores minus one
+#' @param variables default: c("2m_temperature", "evaporation",
+#'   "potential_evaporation", "precipitation_type", "runoff",
+#'   "sub_surface_runoff", "surface_runoff", "total_precipitation")
 #' @param dataset_short_name "reanalysis-era5-single-levels"
 #' @param product_type  default: "reanalysis"
-#' @param years character vector of years (default:  as.character(seq(2010, 2020)))
-#' @param area area coordinates in latitude/longitude (default: c(40, 116, 39, 117))
+#' @param years character vector of years (default:  as.character(seq(2010,
+#'   2020)))
+#' @param area area coordinates in latitude/longitude (default: c(40, 116, 39,
+#'   117))
 #' @param file_format "grib" or "netcdf"
 #' @param export_dir default: "."
 #' @return list with paths to downloaded files
@@ -75,45 +80,51 @@ copernicus_cds <- function(variable = "2m_temperature",
 #' @importFrom kwb.utils catAndRun
 #' @importFrom parallel detectCores parLapply makeCluster stopCluster
 #'
-copernicus_cds_parallel <- function(variables = c("2m_temperature",
-                                                  "evaporation",
-                                                  "potential_evaporation",
-                                                  "precipitation_type",
-                                                  "runoff",
-                                                  "sub_surface_runoff",
-                                                  "surface_runoff",
-                                                  "total_precipitation"),
-                                    dataset_short_name = "reanalysis-era5-single-levels",
-                                    product_type = "reanalysis",
-                                    years = as.character(seq(2010, 2020)),
-                                    area = c(40, 116, 39, 117),
-                                    file_format = "grib",
-                                    export_dir = ".") {
-
+copernicus_cds_parallel <- function(
+  variables = c(
+    "2m_temperature",
+    "evaporation",
+    "potential_evaporation",
+    "precipitation_type",
+    "runoff",
+    "sub_surface_runoff",
+    "surface_runoff",
+    "total_precipitation"
+  ),
+  dataset_short_name = "reanalysis-era5-single-levels",
+  product_type = "reanalysis",
+  years = as.character(seq(2010, 2020)),
+  area = c(40, 116, 39, 117),
+  file_format = "grib",
+  export_dir = "."
+)
+{
   ncores <- parallel::detectCores() - 1
 
   cl <- parallel::makeCluster(ncores)
 
-  msg <- sprintf("Importing %d variable(s) (%s) from Copernicus CDS parallel (ncores = %d)",
-                 length(variables),
-                 paste0("'", paste(variables, collapse = "\', \'"), "'"),
-                 ncores)
-
+  msg <- sprintf(
+    "Importing %d variable(s) (%s) from Copernicus CDS parallel (ncores = %d)",
+    length(variables),
+    paste0("'", paste(variables, collapse = "\', \'"), "'"),
+    ncores
+  )
 
   kwb.utils::catAndRun(
     messageText = msg,
     expr = parallel::parLapply(
       cl, variables, function(variable) {
-        try(copernicus_cds(variable,
-                           dataset_short_name = dataset_short_name ,
-                           product_type = product_type,
-                           years = years,
-                           area = area,
-                           file_format = file_format,
-                           export_dir = export_dir))
+        try(copernicus_cds(
+          variable,
+          dataset_short_name = dataset_short_name ,
+          product_type = product_type,
+          years = years,
+          area = area,
+          file_format = file_format,
+          export_dir = export_dir
+        ))
       })
   )
 
   parallel::stopCluster(cl)
 }
-
