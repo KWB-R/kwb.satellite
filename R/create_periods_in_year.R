5739d1998e327cfcc6fae13dafffd1a13f91b90b
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-22 08:48:59 +0200

    Fix :bug: in create_periods_in_year()
    The last day of the year needs to be removed
    from the vector of period starts

--- R/create_periods_in_year.R
+++ R/create_periods_in_year.R
@@ -1,20 +1,26 @@
 #' Helper function: create periods in year
 #'
 #' @param year year (e.g. 2020)
-#' @param n_periods number of periods into which to separate the year
+#' @param n_periods number of periods into which to separate the year.
+#'   Default: 4L
 #' @return data frame with columns "start" and "end"
 #' @keywords internal
 #' @importFrom kwb.utils startsToEnds
 #' @examples
 #' create_periods_in_year(2018, n_periods = 4L)
-create_periods_in_year <- function(year, n_periods)
+create_periods_in_year <- function(year, n_periods = 4L)
 {
+  stopifnot(length(n_periods) == 1L, n_periods > 0L)
+
   as_date <- function(x) as.Date(sprintf("%04d-%s", as.integer(year), x))
-  from <- as_date("01-01")
-  to <- as_date("12-31")
 
-  starts <- seq(from, to, length.out = n_periods + 1L)
-  ends <- kwb.utils::startsToEnds(starts, lastStop = to)
+  n_dates <- n_periods + 1L
+  dates <- seq.Date(as_date("01-01"), as_date("12-31"), length.out = n_dates)
 
-  data.frame(start = starts, end = ends)
+  starts <- dates[-n_dates]
+
+  data.frame(
+    start = starts,
+    end = kwb.utils::startsToEnds(starts, lastStop = dates[n_dates])
+  )
 }
