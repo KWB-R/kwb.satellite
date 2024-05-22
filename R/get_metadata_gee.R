be4c6d9b9537246a26d004056e89f23ae4db44b6
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-22 07:26:38 +0200

    Clean get_metadata_gee.R
    - use pipe operator (more often)
    - loop through list elements x instead of list
      indices idx, where possible
    - rename "idx" to "i"
    - use helper variable "geometries"

--- R/get_metadata_gee.R
+++ R/get_metadata_gee.R
@@ -1,51 +1,47 @@
 #' Helper function: convert to list
 #'
-#' @param coords column with satellite metadata coordinates (system:footprint`$coordinates)
-#'
+#' @param coords column with satellite metadata coordinates
+#'   (system:footprint`$coordinates)
 #' @return list of coordinates
 #' @keywords internal
 #'
-convert_to_list <- function(coords) {
-
-  tmp_mat <- lapply(seq_along(coords),
-                    FUN = function(idx) {
-
-                      t(as.matrix(coords[[idx]]))
-                    })
-
-  list(do.call(rbind, tmp_mat))
+convert_to_list <- function(coords)
+{
+  lapply(coords, function(x) t(as.matrix(x))) %>%
+    do.call(what = rbind) %>%
+    list()
 }
-
 
 #' Google Earth Engine: get metadata for collection
 #'
 #' @param collection collection
-#'
 #' @return tibble with metadata. In addition also the complex column
-#' "geometry_meta_org" is simplified and stored in the new column "geometry_meta_cleaned"
+#'   "geometry_meta_org" is simplified and stored in the new column
+#'   "geometry_meta_cleaned"
 #' @export
 #' @importFrom tibble as_tibble tibble
 #' @importFrom dplyr bind_cols bind_rows
+#' @importFrom kwb.utils removeColumns
 #' @importFrom tidyr nest
-#' @importFrom tidyselect matches all_of
-gee_get_metadata <- function(collection) {
+#' @importFrom tidyselect matches
+gee_get_metadata <- function(collection)
+{
+  metadata_list <- collection$size()$getInfo() %>%
+    collection$toList() %>%
+    collection$fromImages()
 
-  nImages <- collection$size()$getInfo()
-
-  collectionList <- collection$toList(nImages)
-
-  metadata_list <- collection$fromImages(collectionList)
   metadata_list <- metadata_list$getInfo()
 
-
-  metadata_features_df <- lapply(seq_along(metadata_list$features), function(idx) {
-    tibble::as_tibble(metadata_list$features[[idx]]$properties)
-  }) %>% dplyr::bind_rows() %>%
+  metadata_features_df <- metadata_list$features %>%
+    lapply(function(x) tibble::as_tibble(x$properties)) %>%
+    dplyr::bind_rows() %>%
     tidyr::nest(geometry_meta_org = tidyselect::matches("system:footprint"))
 
+  geometries <- metadata_features_df$geometry_meta_org
 
-  coords <- lapply(seq_len(nrow(metadata_features_df)), function(idx) {
-    convert_to_list(metadata_features_df$geometry_meta_org[[idx]]$`system:footprint`$coordinates) %>%
+  coords <- lapply(seq_len(nrow(metadata_features_df)), function(i) {
+    geometries[[i]]$`system:footprint`$coordinates %>%
+      convert_to_list() %>%
       sf::st_polygon() %>%
       sf::st_sfc() %>%
       sf::st_set_crs(value = 4326)
@@ -53,26 +49,24 @@ gee_get_metadata <- function(collection) {
 
   metadata_features_df$geometry_meta_cleaned <- coords
 
-  metadata_ids <- tibble::tibble(
-    id = sapply(seq_along(metadata_list$features), function(idx) metadata_list$features[[idx]]$id)
-  )
-
-  dplyr::bind_cols(metadata_ids, metadata_features_df) %>%
-    tidyr::separate(col = "id",
-                    into = c("provider_name", "provider_collection", "id_short"),
-                    sep = "/",
-                    remove = FALSE) %>%
+  tibble::tibble(
+    id = sapply(metadata_list$features, function(x) x$id)
+  ) %>%
+    dplyr::bind_cols(metadata_features_df) %>%
+    tidyr::separate(
+      col = "id",
+      into = c("provider_name", "provider_collection", "id_short"),
+      sep = "/",
+      remove = FALSE
+    ) %>%
     tidyr::separate(
       col = "id_short",
-      into = c("datetime_start",
-               "datetime_end",
-               "tile_id"),
+      into = c("datetime_start", "datetime_end", "tile_id"),
       sep = "_"
     ) %>%
     dplyr::mutate(
       datetime_start = lubridate::ymd_hms(datetime_start),
       datetime_end = lubridate::ymd_hms(datetime_end)
     ) %>%
-    dplyr::select(- tidyselect::all_of(c("provider_name","provider_collection")))
-
+    kwb.utils::removeColumns(c("provider_name", "provider_collection"))
 }
