0d8cd7376379022bdfecd14ac2b15228669abcdf
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-05-22 07:44:58 +0200

    Clean import_rds()

--- R/import_rds.R
+++ R/import_rds.R
@@ -1,19 +1,13 @@
 #' Helper function: import .rds files from folder
 #'
 #' @param rds_dir path to directoy containing .rds files
-#'
 #' @return list with imported
 #' @export
 #' @importFrom kwb.utils removeExtension
 #' @importFrom stats setNames
-import_rds <- function(rds_dir) {
-
-rds_paths <- list.files(rds_dir, pattern = "\\.rds$", full.names = TRUE)
-
-rds_names <- kwb.utils::removeExtension(basename(rds_paths))
-
-stats::setNames(lapply(rds_paths, readRDS),
-                nm = rds_names)
+import_rds <- function(rds_dir)
+{
+  files <- list.files(rds_dir, pattern = "\\.rds$", full.names = TRUE)
+  objects <- lapply(files, readRDS)
+  stats::setNames(objects, kwb.utils::removeExtension(basename(files)))
 }
-
-
