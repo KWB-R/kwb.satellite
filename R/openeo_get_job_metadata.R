009bf531e1b459feda0560a6f708799714ddf322
mrustl <michael.rustler@kompetenz-wasser.de>
2024-05-29 13:45:11 +0200

    openeo_jobs_metadata: cast data types

--- R/openeo_get_job_metadata.R
+++ R/openeo_get_job_metadata.R
@@ -7,6 +7,10 @@
 #' @export
 #' @importFrom openeo describe_job
 #' @importFrom kwb.utils catAndRun
+#' @importFrom tidyselect ends_with matches
+#' @importFrom lubridate as_datetime
+#' @importFrom dplyr across mutate
+#' @importFrom tibble as_tibble
 openeo_get_job_metadata <- function(job_id, debug = TRUE) {
 
 kwb.utils::catAndRun(sprintf("Getting job metadata for job id '%s'",
@@ -15,7 +19,13 @@ kwb.utils::catAndRun(sprintf("Getting job metadata for job id '%s'",
 job_info <- openeo::describe_job(job_id)
 cols <- which(names(job_info) != "process")
 
-tibble::as_tibble(t(unlist(job_info[cols])))
+meta <- tibble::as_tibble(t(unlist(job_info[cols]))) %>%
+  dplyr::mutate(dplyr::across(tidyselect::matches("progress"), as.double)) %>%
+  dplyr::mutate(dplyr::across(tidyselect::matches("costs"), as.integer)) %>%
+  dplyr::mutate(dplyr::across(tidyselect::ends_with("value"), as.numeric)) %>%
+  dplyr::mutate(created = lubridate::as_datetime(created, tz = "UTC"),
+                updated = lubridate::as_datetime(updated, tz = "UTC"))
+
 },
 dbg = debug)
 }
